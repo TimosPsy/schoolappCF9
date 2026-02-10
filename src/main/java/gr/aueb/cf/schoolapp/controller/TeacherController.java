a23f2707831930a93758b0d12b951b320d519bbd
Timos <psychoson91@gmail.com>
2026-02-10 12:41:43 +0200

    Initial commit: Spring Boot schoolapp

---
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -0,0 +1,80 @@
+package gr.aueb.cf.schoolapp.controller;
+
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.service.IRegionService;
+import gr.aueb.cf.schoolapp.service.ITeacherService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/teachers")
+@RequiredArgsConstructor
+public class TeacherController {
+
+
+    private final ITeacherService teacherService;
+    private final IRegionService regionService;
+
+    @GetMapping("/insert")
+    public String getTeacherForm(Model model) {
+        model.addAttribute("teacherInsertDTO", TeacherInsertDTO.empty());
+        // model.addAttribute("regionsReadOnlyDTO", regions());
+        return "teacher-insert";
+    }
+
+    @PostMapping("/insert")
+    public String teacherInsert(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO,
+                                BindingResult bindingResult, Model model,
+                                RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            // model.addAttribute("regionsReadOnlyDTO", regions());
+            return "teacher-insert";
+        }
+
+        try {
+            // save teacher to DB
+            //TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO("acfd-1234", "Αθανάσιος", "Ανδρούτσος");
+            //model.addAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+            TeacherReadOnlyDTO teacherReadOnlyDTO = teacherService.saveTeacher(teacherInsertDTO);
+            // PRG - Post-Redirect-Get
+            redirectAttributes.addFlashAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+            return "redirect:/teachers/success";
+
+        }catch (EntityAlreadyExistsException | EntityInvalidArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "teacher-insert";
+
+        }
+
+    }
+
+    @GetMapping("/success")
+    public String teacherSuccess(Model model) {
+        return "teacher-success";
+    }
+
+
+    @ModelAttribute("regionsReadOnlyDTO")       // Εκτελείται πριν από κάθε request handler
+    public List<RegionReadOnlyDTO> regions() {
+//        return List.of(
+//                new RegionReadOnlyDTO(1L, "Αθήνα"),
+//                new RegionReadOnlyDTO(2L, "Βόλος"),
+//                new RegionReadOnlyDTO(3L, "Θεσσαλονίκη"));
+        return regionService.findAllRegionsSortedByName();
+    }
+}
