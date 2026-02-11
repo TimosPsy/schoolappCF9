617a731adc98efd99b5549728555d738e9507f1a
Timos <psychoson91@gmail.com>
2026-02-11 15:43:20 +0200

    fix: add uuid

---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherEditDTO.java
@@ -0,0 +1,31 @@
+package gr.aueb.cf.schoolapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public record TeacherEditDTO(
+
+
+        @NotNull
+        UUID uuid,
+        @NotNull//(message = "Το όνομα δεν μπορεί να είναι κενό")
+        @Size(min = 2)//(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
+        String firstname,
+
+        @NotNull//(message = "Το επώνυμο δεν μπορεί να είναι κενό")
+        @Size(min = 2)//(min = 2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
+        String lastname,
+
+        @Pattern(regexp = "\\d{9,}")//, message = "Το ΑΦΜ δεν μπορεί να είναι μικρότερο από εννέα ψηφία.")
+        String vat,
+
+        @NotNull//(message = "Η περιοχή δεν μπορεί να είναι κενή.")
+        Long regionId
+) {
+    public static TeacherEditDTO empty() {
+        return new TeacherEditDTO(null,"", "", "", 0L);
+    }
+}
